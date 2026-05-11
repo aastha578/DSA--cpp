81e1342126473bdd5bb0e28a9e41699d07b15b22
unknown <you@example.com>
2026-05-11 16:30:00 +0530

    Added segregate linked list solution

---
+++ DSA/LEETCODE/Segregate0_1LL.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct Node {
+ *     int data;
+ *     Node* next;
+ *     Node(int x) {
+ *         data = x;
+ *         next = NULL;
+ *     }
+ * };
+ */
+
+class Solution {
+public:
+    Node* segregate(Node* head) {
+
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        Node* zeroHead = new Node(-1);
+        Node* oneHead  = new Node(-1);
+        Node* twoHead  = new Node(-1);
+
+        Node* zero = zeroHead;
+        Node* one  = oneHead;
+        Node* two  = twoHead;
+
+        Node* temp = head;
+
+        while (temp != NULL) {
+
+            if (temp->data == 0) {
+                zero->next = temp;
+                zero = zero->next;
+            }
+            else if (temp->data == 1) {
+                one->next = temp;
+                one = one->next;
+            }
+            else {
+                two->next = temp;
+                two = two->next;
+            }
+
+            temp = temp->next;
+        }
+
+        zero->next = (oneHead->next) ? oneHead->next : twoHead->next;
+
+        one->next = twoHead->next;
+
+        two->next = NULL;
+
+        head = zeroHead->next;
+
+        delete zeroHead;
+        delete oneHead;
+        delete twoHead;
+
+        return head;
+    }
+};
